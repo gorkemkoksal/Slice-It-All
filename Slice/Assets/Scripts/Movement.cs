a5c912900cc7864a4078daca195ea9551566456e
gorkemkoksal <81961582+gorkemkoksal@users.noreply.github.com>
2023-03-14 23:20:32 +0300

    knife colliders

--- Slice/Assets/Scripts/Movement.cs
+++ Slice/Assets/Scripts/Movement.cs
@@ -1,18 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody playerRb;
+    private void Awake()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+        }
     }
 }
